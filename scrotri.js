3bf5b879cf2aed0270380f099d115e8fc00d9d1e
Namysh <willytardy@outlook.fr>
2020-10-30 18:45:58 +0100

    ✨ Adding 'once' parameter

--- scrotri.js
+++ scrotri.js
@@ -1,21 +1,27 @@
 function Scrotri(options = {}) {
-    const elements = document.querySelectorAll('.SCTR-init');
-    const windowHeight = window.innerHeight;
+    const _elements = document.querySelectorAll('[data-sctr]');
 
-    options.useAnimateCSS &&
+    options && options.useAnimateCSS &&
         document.head.insertAdjacentHTML(
             'beforeend',
             '<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css"/>'
         );
 
-    window.addEventListener('scroll', function awaitingAnimation() {
-        [...elements].forEach((element) => {
+    const _handleScroll = () => {
+        [..._elements].forEach(element => {
+            const windowHeight = window.innerHeight;
             const posFromTop = element.getBoundingClientRect().top;
+
             if (posFromTop - windowHeight <= 0)
-                element.className = element.className.replace(
-                    'SCTR-init',
-                    'animated ' + element.dataset.sctr || 'undefined'
+                element.classList.add(
+                    'animated',
+                    element.dataset.sctr || 'undefined'
                 );
+            else
+                element.dataset.sctrOnce ||
+                    element.classList.remove('animated', element.dataset.sctr);
         });
-    });
+    };
+
+    window.addEventListener('scroll', _handleScroll);
 }
